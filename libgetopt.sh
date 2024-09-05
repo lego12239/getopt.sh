97bec1ab971bac3750b4ad26b07222e0edb4bb3f
Oleg Nemanov <lego12239@yandex.ru>
2024-09-05 20:04:28 +0300

    update copyright info

--- libgetopt.sh
+++ libgetopt.sh
@@ -2,7 +2,7 @@
 
 #
 # GETOPT CODE START
-# Copyright (c) 2023 Oleg Nemanov <lego12239@yandex.ru>
+# Copyright (c) 2023-2024 Oleg Nemanov <lego12239@yandex.ru>
 # Version: 1.2
 # SPDX-License-Identifier: BSD-2-Clause
 #
